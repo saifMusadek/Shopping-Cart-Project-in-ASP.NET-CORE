c8b74f28b609b702fd1f1d8096fc306a1bd79a62
Saifullah <truesaif111@gmail.com>
2023-09-13 11:07:48 +0600

    added to repo

---
+++ BulkyBook.Web/Controllers/CatagoryController.cs
@@ -0,0 +1,130 @@
+﻿using BulkyBook.Web.Data;
+using BulkyBook.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Web.Controllers
+{
+    public class CatagoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CatagoryController(ApplicationDbContext db) 
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Catagory> objCatagoryList = _db.Catagories.ToList();
+            return View(objCatagoryList);
+        }
+
+
+        //Get
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Catagory obj) {
+            if (obj.Name == obj.DisplayOrder.ToString()) {
+                ModelState.AddModelError("name","The DisplayOrder cannot exactly match the Name");
+            
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Catagories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+            
+            
+        }
+
+        public IActionResult Edit(int? id)
+            //show existing catagory whle loading page
+        {
+            if (id == null || id ==0)
+            {
+                return NotFound();
+            }
+
+            var catagoryFromDb = _db.Catagories.Find(id);
+
+            if (catagoryFromDb == null) {
+                return NotFound();
+            }
+
+
+            return View(catagoryFromDb);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Catagory obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Catagories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+
+        }
+
+        public IActionResult Delete(int? id)
+        //show existing catagory whle loading page
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var catagoryFromDb = _db.Catagories.Find(id);
+
+            if (catagoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(catagoryFromDb);
+        }
+
+        //Post
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+           var obj = _db.Catagories.Find(id);
+            if (obj == null) {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Catagories.Remove(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category deleted successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+
+        }
+    }
+}
---
+++ BulkyBook.Web/obj/Release/net6.0/win-x86/PubTmp/EFSQLScripts/BulkyBook.Web.Data.ApplicationDbContext.sql
@@ -0,0 +1,35 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230912082034_AddCatagoryToDatabase')
+BEGIN
+    CREATE TABLE [Catagories] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [DisplayOrder] int NOT NULL,
+        [CreatedDateTime] datetime2 NOT NULL,
+        CONSTRAINT [PK_Catagories] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230912082034_AddCatagoryToDatabase')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20230912082034_AddCatagoryToDatabase', N'7.0.10');
+END;
+GO
+
+COMMIT;
+GO
+
